026403955b09b71cf704c16bbd800d657ad4684d
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-03-09 11:45:56 +0300

    ain

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,62 @@
 #!/usr/bin/env python3
 
+#from typing import Self
+
+# class TestBook:
+#     '''Book in book.py'''
+
+#     def test_has_title_and_page_count(self):
+#         '''has the title and page_count passed into __init__, and values can be set to new instance.'''
+#         book = Book("And Then There Were None", 272)
+#         assert(book.page_count == 272)
+#         assert(book.title == "And Then There Were None")
+
+#     def test_requires_int_page_count(self):
+#         '''prints "page_count must be an integer" if page_count is not an integer.'''
+#         book = Book("And Then There Were None", 272)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         book.page_count = "not an integer"
+#         sys.stdout = sys.__stdout__
+#         assert captured_out.getvalue() == "page_count must be an integer\n"
+
+#     def test_can_turn_page(self):
+#         '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
+#         book = Book("The World According to Garp", 69)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         book.turn_page()
+#         sys.stdout = sys.__stdout__
+#         assert(captured_out.getvalue() == "Flipping the page...wow, you read fast!\n")
+
+
 class Book:
-    pass
+    def __init__(self,title="How ",page_count=9) :
+        self.title=title
+        self.page_count=page_count
+
+    @property
+    def get_title(self):
+        return self._title
     
+    @get_title.setter
+    def get_title(self,title):
+      self._title=title
+
+    @property   
+    def get_page_count(self):
+        return self._page_count
         
+    @get_page_count.setter
+    def set_page_count(self,page_count):
+        if isinstance(page_count,int):
+            self._page_count=page_count
+        else:raise ValueError("Page_count must be an integer")
+
+    def turn_page(self):
+     print("Flipping the page...wow, you read fast!")
+     return "Flipping the page...wow, you read fast!"
+    
+    #page_count=property(get_page_count)
+Rails=Book("Rails",9)
+print(Rails.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,85 @@
 #!/usr/bin/env python3
+# class TestShoe:
+#     '''Shoe in shoe.py'''
+
+#     def test_has_brand_and_size(self):
+#         '''has the brand and size passed to __init__, and values can be set to new instance.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         assert(stan_smith.brand == "Adidas")
+#         assert(stan_smith.size == 9)
+
+#     def test_requires_int_size(self):
+#         '''prints "size must be an integer" if size is not an integer.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         stan_smith.size = "not an integer"
+#         sys.stdout = sys.__stdout__
+#         assert captured_out.getvalue() == "size must be an integer\n"
+
+#     def test_can_cobble(self):
+#         '''says that the shoe has been repaired.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         stan_smith.cobble()
+#         sys.stdout = sys.__stdout__
+#         assert(captured_out.getvalue() == "Your shoe is as good as new!\n")
+    
+#     def test_cobble_makes_new(self):
+#         '''creates an attribute on the instance called 'condition' and set equal to 'New' after repair.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         stan_smith.cobble()
+#         assert(stan_smith.condition == "New")
+        
+        
+   
+
 
 class Shoe:
-    pass
+    def __init__(self,brand="Adidas",size=7) :
+        self.brand=brand
+        self.size=size
+        
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self,brand):
+        self._brand=brand
+
+    @property 
+    def size(self):
+        return self._brand
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):        
+        print("Your shoe is as good as new!")
+        self.condition="New"
+
+    #@property
+    # def condition(self):
+    #     print("Your shoe is as good as new")
+    #     return self._condition
+    
+    # @condition.setter
+    # def condition(self,condition="New"):
+    #     self._condition=condition
+        
+
+Adidas=Shoe("Puma",10)
+print(Adidas._brand)
+print(Adidas.cobble())
+
+
+
+               
+       
